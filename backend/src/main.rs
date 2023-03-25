97f9db41f9aa3a368f845a3c4fd9371bc1ab3822
Lars Mitsem Selbekk <lars@selbekk.com>
2023-03-25 15:26:25 +0100

    Set up axum hello-world
    Closes #1

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use ::gunboat_bingo_backend::get_app;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = get_app();
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
